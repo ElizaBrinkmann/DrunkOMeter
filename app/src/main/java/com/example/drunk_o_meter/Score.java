e32c5ed57ced4ea02cf68f47efffe9f3ff9e5794
melindav <melindav@metropolia.fi>
2019-02-17 23:51:04 +0200

    made class Score

---
+++ app/src/main/java/com/example/drunk_o_meter/Score.java
@@ -0,0 +1,15 @@
+package com.example.drunk_o_meter;
+
+public class Score {
+    private int score;
+public Score(){
+    this.score=0;
+    }
+public void addToScore(int i){
+    this.score+=i;
+}
+
+    public int getScore() {
+        return this.score;
+    }
+}
