d370e97e1031aee07c8a75d84429fea398aaf5d7
Chavilaw Wong <chavilahchanaw@gmail.com>
2019-03-07 11:37:19 +0200

    Added the drink listing view

---
+++ app/src/main/java/com/example/drunk_o_meter/DrinkList.java
@@ -0,0 +1,13 @@
+package com.example.drunk_o_meter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_list);
+    }
+}
