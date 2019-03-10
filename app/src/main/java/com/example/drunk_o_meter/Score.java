fc44ae6fcedcad19bf9f89ea337461802e4b0824
melindav <melindav@metropolia.fi>
2019-03-10 11:49:06 +0200

    , fixed small mistake

--- app/src/main/java/com/example/drunk_o_meter/Score.java
+++ app/src/main/java/com/example/drunk_o_meter/Score.java
@@ -30,7 +30,7 @@ public class Score {
      public String giveAdvise() {
         if (this.score <= 3) {
             return ("You are not drunk!");
-        } else if (this.score > 3 && this.score <) {
+        } else if (this.score > 3 && this.score <6) {
             return ("Drink some water and eat something and you can drink more!");
         } else if (this.score >= 6) {
             //about 6 beers in 5 hours
