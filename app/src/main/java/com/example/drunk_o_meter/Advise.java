5593a4b4003457ec28a590ce6ac6c1fb05ab0ba3
Eliza Brinkmann <eliza.brinkmann@metropolia.fi>
2019-03-11 22:21:36 +0200

    Try to show the previous scores

--- app/src/main/java/com/example/drunk_o_meter/Advise.java
+++ app/src/main/java/com/example/drunk_o_meter/Advise.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Advise extends AppCompatActivity {
     AdvisePage advicePage = new AdvisePage();
+
+
+    // SimpleDateFormat sdf = new SimpleDateFormat("d MMM yyyy HH:mm", Locale.getDefault());
 
     @Override
     //placing the advice in adviceView
@@ -20,12 +28,25 @@ public class Advise extends AppCompatActivity {
 
         TextView sv = findViewById(R.id.textViewScore);
         sv.setText(String.valueOf(ScoreSingleton.getInstance().getScore()));
+
+        TinyDB tinydb = new TinyDB(this);
+        ArrayList<String> scores = tinydb.getListString("key");
+
+        scores.add("Score: " + ScoreSingleton.getInstance().toString());
+
+        tinydb.putListString("key", scores);
+
+        Spinner spinner = findViewById(R.id.simple_spinner_dropdown_item);
+        //new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, tinydb.getListString("key"));
+        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, tinydb.getListString("key")));
+
     }
 
 //when pressed starts the test again
     public void onClick (View view) {
-        ScoreSingleton.getInstance().restartScore();
+
         Intent nextActivity = new Intent(Advise.this, MainActivity.class);//moves from Advise page to MainActivity
+        ScoreSingleton.getInstance().restartScore();
         startActivity(nextActivity);//starting new activity
     }
 
